fd9e46c91f1961fdbae42a3b3d26cf6247e37ac8
america-rg <ame.rg@outlook.com>
2024-11-22 11:13:18 -0600

    endpoint de cookie

--- routes/public-routes.js
+++ routes/public-routes.js
@@ -9,3 +9,11 @@ publicRouter.post('logout', (req, res) => {
 })
 publicRouter.post('/login', UsuariosController.matchUsuario)
 publicRouter.post('/', UsuariosController.create)
+
+publicRouter.get('/cookie', (req, res) => {
+  const token = req.cookies.acceso_token; // Obtener la cookie
+  if (token) {
+    return res.json({ token }); // Enviar la cookie como JSON
+  }
+  return res.status(404).json({ message: 'Cookie no encontrada' });
+});
